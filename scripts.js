15aab4485db1d0e129afbd2d1a2ed415bfa341f2
colorwing-ship-it <angel19780503@gmail.com>
2025-12-18 09:34:57 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ fetch("/products.json")
       card.className = "product-card";
 
       const img = document.createElement("img");
-      img.src = `images/${product.folder}/${product.folder}_001.jpg`;
+      img.src = `../images/${product.folder}/${product.folder}_001.jpg`;
       img.alt = product.name_en;
 
       const title = document.createElement("div");
